34ce4227e9af070f2498bb5b0c796c9eaedfc9d7
Tavo Cruz <madecruz2@gmail.com>
2023-09-16 22:03:52 -0700

    start contact component

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import MapIcon from '@mui/icons-material/Map';
+import EmailIcon from '@mui/icons-material/Email';
 const ContactMe = () => {
   return (
-    <div>ContactMe</div>
+    <div className="contactMe__wrapper">
+      <div className="contact__me__top">
+        <h3>Contact</h3>
+        <div className="contact__me__icon">
+          <h2>Don't be shy! Contact Me!</h2> <ConnectWithoutContactIcon />
+        </div>
+      </div>
+      <div className="contact__me__bottom">
+        <div className="contact__location">
+          <MapIcon />
+          <div className="contact__location__city">
+            <h3>Location</h3>
+            <p>Hayward, CA</p>
+          </div>
+        </div>
+        <div className="contact__email">
+          <EmailIcon />
+          <div className="contact__email__info">
+            <h3>Email</h3>
+            <p>cruzortegaoctavio@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
