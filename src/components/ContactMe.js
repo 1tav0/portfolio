e1b44ff2c52141163b586ef98c45a87f55f370b0
Tavo Cruz <madecruz2@gmail.com>
2023-09-16 22:33:31 -0700

    finish contact me component

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -2,32 +2,38 @@ import React from 'react'
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
 import MapIcon from '@mui/icons-material/Map';
 import EmailIcon from '@mui/icons-material/Email';
+import './ContactMe.css';
+
 const ContactMe = () => {
   return (
-    <div className="contactMe__wrapper">
-      <div className="contact__me__top">
-        <h3>Contact</h3>
-        <div className="contact__me__icon">
-          <h2>Don't be shy! Contact Me!</h2> <ConnectWithoutContactIcon />
-        </div>
-      </div>
-      <div className="contact__me__bottom">
-        <div className="contact__location">
-          <MapIcon />
-          <div className="contact__location__city">
-            <h3>Location</h3>
-            <p>Hayward, CA</p>
+    <section id="contactme">
+      <div className="contactMe__wrapper">
+        <div className="contact__me__parent">
+          <div className="contact__me__top">
+            <h3>Contact</h3>
+            <div className="contact__me__icon">
+              <h2>Don't be shy! Contact Me!</h2> <ConnectWithoutContactIcon />
+            </div>
+          </div>
+          <div className="contact__me__bottom">
+            <div className="contact__location">
+              <MapIcon />
+              <div className="contact__location__city">
+                <h3>Location</h3>
+                <p>Hayward, CA</p>
+              </div>
+            </div>
+            <div className="contact__email">
+              <EmailIcon />
+              <div className="contact__email__info">
+                <h3>Email</h3>
+                <p>cruzortegaoctavio@gmail.com</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="contact__email">
-          <EmailIcon />
-          <div className="contact__email__info">
-            <h3>Email</h3>
-            <p>cruzortegaoctavio@gmail.com</p>
-          </div>
-        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
