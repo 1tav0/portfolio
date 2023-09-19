afb1acae952a6298f29865450cd6b722b65f9a45
Tavo Cruz <madecruz2@gmail.com>
2023-09-19 11:17:41 -0700

    modify about me component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <Intro/>
+      {/* <Intro/> */}
       <AboutMe />
       <Skills />
       <Projects />
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,26 +12,30 @@ const AboutMe = () => {
   return (
     <section id="aboutme">
       <div className="aboutme">
-            <div className="aboutme__left">
-              <div className="aboutme__info">
-                  <h1>Hello</h1>
-                  <h3>My Name is Octavio Cruz Ortega</h3>
-                  <p> I am a Full Stack Developer with expertise in the MERN stack, as well as Firebase and its emulators for creating full-stack applications. My focus is building real-life web applications. I have extensive knowledge of JavaScript, Node.js, C++, React, Express, Firebase, Redux, HTML, CSS, and Styled Components.
-                      I enjoy engaging in problem-solving, and implementing popular algorithms from websites like LeetCode. I love creating modern, and user-friendly applications that solve real-life problems through software.
-                      Overcoming the adversities that come with each project is the most exciting part for me, and achieving the desired results gives me a great sense of accomplishment.
-                  </p>
-                  <div className="aboutme__info__icons">
-                    <a href={githubProfileUrl} target="_blank" rel="noopener noreferrer" >
-                      <GitHubIcon />
-                    </a>
-                    <YouTubeIcon />
-                    <a href={LinkedIn} target="_blank" rel="noopener noreferrer">
-                      <LinkedInIcon />
-                    </a>
-                  </div>
-              </div>
-              <img src={result} alt="" />
+        <div className="aboutme__name">
+          <h1>Hello</h1>
+          <h3>I am Octavio Cruz Ortega</h3>
+        </div>
+        <div className="aboutme__wrapper">
+          <div className="aboutme__left">
+            <div className="aboutme__info">
+              <p> I am a Full Stack Developer with expertise in the MERN stack, as well as Firebase and its emulators for creating full-stack applications. My focus is building real-life web applications. I have extensive knowledge of JavaScript, Node.js, C++, React, Express, Firebase, Redux, HTML, CSS, and Styled Components.
+                I enjoy engaging in problem-solving, and implementing popular algorithms from websites like LeetCode. I love creating modern, and user-friendly applications that solve real-life problems through software.
+                Overcoming the adversities that come with each project is the most exciting part for me, and achieving the desired results gives me a great sense of accomplishment.
+              </p>
             </div>
+            <div className="aboutme__info__icons">
+              <a href={githubProfileUrl} target="_blank" rel="noopener noreferrer" >
+                <GitHubIcon />
+              </a>
+              <YouTubeIcon />
+              <a href={LinkedIn} target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon />
+              </a>
+            </div>
+          </div>
+          <img className='image' src={result} alt="" />
+        </div>
       </div>
     </section>
   )
